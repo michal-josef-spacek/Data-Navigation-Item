7fbb601be57aca22d6d0470445d603094d722c0b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-02-10 18:00:12 +0100

    Add css_class parameter

--- Item.pm
+++ Item.pm
@@ -5,9 +5,14 @@ use warnings;
 
 use Mo qw(build is);
 use Mo::utils 0.09 qw(check_length check_number check_required);
+use Mo::utils::CSS 0.02 qw(check_css_class);
 use Mo::utils::URI qw(check_url);
 
 our $VERSION = 0.01;
+
+has css_class => (
+	is => 'ro',
+);
 
 has desc => (
 	is => 'ro',
@@ -27,6 +32,9 @@ has url => (
 
 sub BUILD {
 	my $self = shift;
+
+	# Check CSS class.
+	check_css_class($self, 'css_class');
 
 	# Check desc.
 	check_length($self, 'desc', 1000);
